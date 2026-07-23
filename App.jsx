4ff7e055f265663c60c2da38c86c08ec1f8febc9
immanuel <immanuel.okoth@student.moringaschool.com>
2026-07-23 13:35:37 +0300

    added frontend routing

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthProvider';
+import ProtectedRoute from './components/ProtectedRoute';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import SpacesPage from './pages/SpacesPage';
+import ReservationHistoryPage from './pages/ReservationHistoryPage';
+import AdminDashboard from './pages/AdminDashboard';
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/spaces" element={<ProtectedRoute><SpacesPage /></ProtectedRoute>} />
+          <Route path="/history" element={<ProtectedRoute><ReservationHistoryPage /></ProtectedRoute>} />
+          <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
